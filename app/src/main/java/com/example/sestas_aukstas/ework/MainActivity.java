339f555d2c3e982a421912a11118aab1cb938368
tadbil <biliunast@gmail.com>
2018-04-24 13:53:27 +0300

    New working menu layout

--- app/src/main/java/com/example/sestas_aukstas/ework/MainActivity.java
+++ app/src/main/java/com/example/sestas_aukstas/ework/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,42 +17,64 @@ import com.google.firebase.auth.FirebaseUser;
 /**
  * Created by Martynas on 3/21/2018.
  * Modified by Tadas on 4/15/2018.
+ * Modified by Tadas on 4/24/2018.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     FirebaseAuth firebaseObj;
-    private NavigationView navigationView;
+    TextView nav_account;
+    TextView nav_work;
+    TextView nav_settings;
+    TextView nav_logout;
+    private LinearLayout navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        initializeObj();
+        nav_account.setOnClickListener(this);
+        nav_work.setOnClickListener(this);
+        nav_settings.setOnClickListener(this);
+        nav_logout.setOnClickListener(this);
 
-        navigationView = findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.nav_account:
-                        goToProfile();
-                        return true;
-                    case R.id.nav_settings:
-                        goToSettings();
-                        return true;
-                    case R.id.nav_logout:
-                        logOut();
-                        return true;
-                }
+    }
 
-                return true;
-            }
-        });
+    public void initializeObj() {
+        firebaseObj = firebaseObj.getInstance();
+        firebaseObj = FirebaseAuth.getInstance();
+        nav_account = findViewById(R.id.nav_account);
+        nav_work = findViewById(R.id.nav_work);
+        nav_settings = findViewById(R.id.nav_settings);
+        nav_logout = findViewById(R.id.nav_logout);
+    }
+
+    public void onClick(View v) {
+        if (v == nav_account) {
+            goToProfile();
+        }
+        if(v == nav_work)
+        {
+            goToWork();
+        }
+        if(v == nav_settings){
+            goToSettings();
+        }
+
+        if (v == nav_logout) {
+            logOut();
+        }
+
 
     }
 
     public void goToProfile(){
         startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+    }
+
+    public void goToWork(){
+        startActivity(new Intent(MainActivity.this, WorkActivity.class));
     }
 
     public void goToSettings(){
@@ -62,11 +86,6 @@ public class MainActivity extends AppCompatActivity {
         //  firebaseObj.signOut();
         Toast.makeText(MainActivity.this, "Sėkmingai atsijungta", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
-    }
-
-
-    public void initializeObj() {
-        firebaseObj = firebaseObj.getInstance();
     }
 
 }
