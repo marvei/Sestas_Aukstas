695b34134520ae961ae2bda71055ac766376a997
tadbil <biliunast@gmail.com>
2018-05-02 14:01:26 +0300

    Profile settings is synced with firebase, values are updating

--- app/src/main/java/com/example/sestas_aukstas/ework/ProfileSettingsActivity.java
+++ app/src/main/java/com/example/sestas_aukstas/ework/ProfileSettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.sestas_aukstas.ework;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +27,7 @@ public class ProfileSettingsActivity extends AppCompatActivity implements View.O
     private Button btnSave;
     private FirebaseAuth firebaseObj;
     private FirebaseDatabase database;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,7 @@ public class ProfileSettingsActivity extends AppCompatActivity implements View.O
     }
 
     public void initializeObj() {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         firebaseObj = FirebaseAuth.getInstance();
         newName = findViewById(R.id.etNameUpdate);
         showUserName(firebaseObj.getCurrentUser());
@@ -44,15 +49,11 @@ public class ProfileSettingsActivity extends AppCompatActivity implements View.O
 
     public void onClick(View v) {
         if (v == btnSave) {
-           // saveChanges();
-            Toast.makeText(ProfileSettingsActivity.this, "Pakeitimai išsaugoti.", Toast.LENGTH_SHORT).show();
+            if (noEmptyFields()){
+                saveChanges(firebaseObj.getCurrentUser());
+            }
+
         }
-    }
-
-    public void saveChanges(FirebaseUser currentUser)
-    {
-
-
     }
 
     public void showUserName(FirebaseUser currentUser){
@@ -79,10 +80,57 @@ public class ProfileSettingsActivity extends AppCompatActivity implements View.O
         });
     }
 
+    public void saveChanges(FirebaseUser currentUser) {
+        changesSuccessful = false;
+        if (noEmptyFields()) {
+            String name = newName.getText().toString();
+            DatabaseReference myRef = database.getReference().child("users").child(currentUser.getUid()).child("vardas");
+            try {
+
+                myRef.setValue(name);
+            } catch (Exception e) {
+                Toast.makeText(ProfileSettingsActivity.this, "Vardo Pakeisti Nepavyko.", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+                finish();
+            }
+
+            final DatabaseReference myRefEmail = database.getReference().child("users").child(currentUser.getUid()).child("email");
+
+            final String userEmailNew = newEmail.getText().toString();
+            final String userEmailOld = firebaseUser.getEmail().toString();
+                firebaseUser.updateEmail(userEmailNew).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            try{
+                                myRefEmail.setValue(userEmailNew);
+                                Toast.makeText(ProfileSettingsActivity.this, "Pakeitimai išsaugoti.", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                            catch (Exception e) {
+
+                                firebaseUser.updateEmail(userEmailOld);
+                                Toast.makeText(ProfileSettingsActivity.this, "El. Pašto Pakeisti Nepavyko.", Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                                finish();
+                            }
+                          // finish();
+                        } else {
+                            Toast.makeText(ProfileSettingsActivity.this, "El. Pašto Pakeisti Nepavyko", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                });
+
+        }
+
+    }
+
     public boolean isDataLoaded(EditText newName, EditText newEmail)
     {
         if(newName.getText() != null && newEmail.getText() != null)
         {
+            newName.requestFocus();
             return true;
         }
         else return false;
